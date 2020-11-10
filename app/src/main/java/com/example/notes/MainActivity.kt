e77d3840dbfe158d297df9eb20d143f10f20b93e
priyanshu <jainpriyanshu341@gmail.com>
2020-11-10 22:59:40 +0530

    keyboard change

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.notes
 
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -40,6 +42,11 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
         if(noteText.isNotEmpty()) {
             viewModel.insertNote(Note(noteText))
             Toast.makeText(this,"$noteText Inserted",Toast.LENGTH_LONG).show()
+            //this two line remove keyboard
+            val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            input.text = null
         }
     }
+
 }
